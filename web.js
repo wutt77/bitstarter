cf67b3466f85b846734e7bb780d6a5f545e42c17
wutt77 <wutt77@gmail.com>
2013-07-10 08:43:31 +0000

    readFile

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var readBuf = new Buffer(256);
+
+readBuf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(readBuf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
